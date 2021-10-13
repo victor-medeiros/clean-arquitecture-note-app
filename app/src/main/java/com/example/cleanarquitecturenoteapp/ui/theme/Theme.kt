ac60c8ed07abda1cb03e6a11619c727c7e8fa282
victor-medeiros <victor.msilva>
2021-10-13 14:18:42 -0300

    Project structure

---
+++ app/src/main/java/com/example/cleanarquitecturenoteapp/ui/theme/Theme.kt
@@ -0,0 +1,28 @@
+package com.example.cleanarquitecturenoteapp.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGray
+)
+
+@Composable
+fun CleanArquitectureNoteAppTheme(
+    darkTheme: Boolean = true,
+    content: @Composable() () -> Unit
+) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
