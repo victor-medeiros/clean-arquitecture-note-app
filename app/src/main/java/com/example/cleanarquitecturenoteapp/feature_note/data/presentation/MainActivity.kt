eff35ec0071a5c6bb65515ada77aa4482d0f8147
victor-medeiros <victor.msilva>
2021-10-13 14:40:24 -0300

    Project structure

--- app/src/main/java/com/example/cleanarquitecturenoteapp/feature_note/data/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cleanarquitecturenoteapp/feature_note/data/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.cleanarquitecturenoteapp
+package com.example.cleanarquitecturenoteapp.feature_note.data.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cleanarquitecturenoteapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
